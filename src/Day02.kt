f44a748ff68eaeeb439963ad89a74551c6a59837
Bjoern Kuhlen <bjoern@kuhlen.org>
2024-12-09 02:06:17 +0100

    day 2 improvement

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,23 +1,25 @@
 import kotlin.math.absoluteValue
 
-class Day02 : Day<Int,List<List<Int>>>(2, 246, 4, 318) {
-    override fun parseInput(input: String): List<List<Int>> = input.lines().filter { it.isNotEmpty() }
+class Day02 : Day<Int,List<Report>>(2, 246, 4, 318) {
+    override fun parseInput(input: String): List<Report> = input.lines().filter { it.isNotEmpty() }
         .map { it.split(" ").map { it.toInt() } }
 
-    override fun part1(input: List<List<Int>>): Int {
-        return input.count { isReportSafe(it) }
+    override fun part1(input: List<Report>): Int {
+        return input.count { it.isSafe() }
     }
 
-    override fun part2(input: List<List<Int>>): Int {
-        return input.count { isReportSafe(it) || it.getAllCombinationsWithOneElementRemoved().any { isReportSafe(it) } }
+    override fun part2(input: List<Report>): Int {
+        return input.count { it.isSafe() || it.getAllCombinationsWithOneElementRemoved().any { it.isSafe() } }
     }
 
-    private fun isReportSafe(report: List<Int>): Boolean {
-        val diffs = report.zipWithNext().map { it.second - it.first }
+    private fun Report.isSafe(): Boolean {
+        val diffs = this.zipWithNext().map { it.second - it.first }
         return (diffs.all { it > 0 } || diffs.all { it < 0 }) && diffs.all { it.absoluteValue in 1..3 }
     }
 
-    fun <T> List<T>.getAllCombinationsWithOneElementRemoved(): List<List<T>> {
+    private fun <T> List<T>.getAllCombinationsWithOneElementRemoved(): List<List<T>> {
         return (indices).map { i -> take(i) + drop(i + 1) }
     }
 }
+
+private typealias Report = List<Int>
