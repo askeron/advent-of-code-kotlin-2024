b22de0df4fbfba6a833a81fc5c7506d31c882a93
Bjoern Kuhlen <bjoern@kuhlen.org>
2024-12-19 13:51:33 +0100

    day 1 simplify

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,6 +1,6 @@
 import kotlin.math.absoluteValue
 
-class Day01 : Day<Int,List<String>>(11, 1765812, 31, 20520794) {
+class Day01 : Day<Int, List<String>>(11, 1765812, 31, 20520794) {
     override fun parseInput(input: String): List<String> = input.lines().filter { it.isNotEmpty() }
 
     private fun getLists(input: List<String>): Pair<List<Int>, List<Int>> = (0..1).toList()
@@ -15,8 +15,8 @@ class Day01 : Day<Int,List<String>>(11, 1765812, 31, 20520794) {
     }
 
     override fun part2(input: List<String>): Int {
-        val lists = getLists(input)
-        val countMap = lists.second.groupingBy { it }.eachCount()
-        return lists.first.sumOf { it * (countMap[it] ?: 0) }
+        val (leftList, rightList) = getLists(input)
+        val countMap = rightList.groupingBy { it }.eachCount()
+        return leftList.sumOf { it * (countMap[it] ?: 0) }
     }
 }
